d42e4d5f2e32c3712592c3d91cf835c13cb68894
MarVidal17 <marvidalsegura@gmail.com>
2021-03-09 13:30:56 +0100

    task 7 initiated

---
+++ 7task/README.md
@@ -0,0 +1,10 @@
+# Task 7
+
+* Entendre diferencia entre deployment/statefulset/cronjob
+* Implementar un codi go que exposi un endpoint. Per cada crida a l'endpoint, aquest ha d'incrementar un contador guardat en un fitxer.
+* Desplegar un deployment fent servir el codi del punt anterior. El deployment ha de ser multireplica i compartir el mateix contador entre les diferents repliques. Juntament amb el deployment, hauras de crear un service.
+* Desplegar un statefulset amb diverses repliques fent servir el codi go anterior. Hauras de crear un headless service.
+* Implementar un codi go que simplement faci un print dient alguna cosa.
+* Desplegar un cronjob fent servir el codi del punt anterior.
+* Crear un helm chart que empaqueti tant el deployment, com l'statefulset i el cronjob
+
---
+++ 7task/counter.go
@@ -0,0 +1,61 @@
+package main
+
+ 
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+	"os"
+    "bufio"
+    "strconv"
+    "strings"
+)
+
+ 
+
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	filename := "counter.txt"
+    var f *os.File
+ 
+    counter := 0
+    _, err := os.Stat(filename)
+    if !os.IsNotExist(err) {
+        f, _ = os.Open(filename)
+        reader := bufio.NewReader(f)
+        var counter_str string
+        counter_str, err := reader.ReadString('\n')
+        if err != nil {
+            fmt.Println("error")
+            fmt.Println(err)
+            os.Exit(1)
+        }
+        counter_str = strings.TrimSuffix(counter_str, "\n")
+        counter, err = strconv.Atoi(counter_str)
+        if err != nil {
+            fmt.Println("error")
+            fmt.Println(err)
+            os.Exit(1)
+        }
+    }
+ 
+    counter++
+ 
+    f, err = os.Create(filename)
+    if err == nil {
+        f.WriteString(fmt.Sprintf("%d\n", counter))
+        f.Close()
+    }
+    fmt.Fprintln(w, "Counter incremented: ", counter)
+}
+
+ 
+
+func main() {
+		log.Println("Counter working!")
+    http.HandleFunc("/", handler)
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatalln(err)
+    }
+}
